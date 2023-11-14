49ccf72cb3ce8a8cf278c841d1aa19a993a1839b
Bilal0313 <Bilalahmat222@gmail.com>
2023-11-13 19:10:48 -0500

    Created first Driver Object

--- src/test/java/com/cydeo/tests/day1_intro/OpenBrowser.java
+++ src/test/java/com/cydeo/tests/day1_intro/OpenBrowser.java
@@ -1,6 +1,16 @@
 package com.cydeo.tests.day1_intro;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class OpenBrowser {
+
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+
+
+
+
+    }
 
 
 }
