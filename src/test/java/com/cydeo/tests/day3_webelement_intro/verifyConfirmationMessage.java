53c1d3f320a4d2d44ae7be4d06b896ec5bb3531a
Bilal0313 <Bilalahmat222@gmail.com>
2023-11-14 00:43:44 -0500

    Update the class

--- src/test/java/com/cydeo/tests/day3_webelement_intro/verifyConfirmationMessage.java
+++ src/test/java/com/cydeo/tests/day3_webelement_intro/verifyConfirmationMessage.java
@@ -1,10 +1,37 @@
 package com.cydeo.tests.day3_webelement_intro;
 
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class verifyConfirmationMessage {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException{
 
 
-        
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.navigate().to("https://practice.cydeo.com/forgot_password");
+
+        String beforeUrl = driver.getCurrentUrl();
+
+        WebElement emailInputBox = driver.findElement(By.name("email"));
+        emailInputBox.sendKeys("Ab@gmail.com");
+
+        Thread.sleep(2000);
+
+        WebElement RetrievePasswordBtn = driver.findElement(By.id("form_submit"));
+        RetrievePasswordBtn.click();
+
+        Thread.sleep(2000);
+
+        WebElement message = driver.findElement(By.id("content"));
+        boolean a = message.getText().equals("Your e-mail's been sent!");
+
+        System.out.println(a);
+
+        // what is getAttribute method?
+
+
 
     }
 }
