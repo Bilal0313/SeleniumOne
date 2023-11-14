c0fbd0d5286588eb31a8599a727b799593fd0e63
Bilal0313 <Bilalahmat222@gmail.com>
2023-11-13 19:01:21 -0500

    Add some faker in to the class

--- src/test/java/com/cydeo/tests/FirstClass.java
+++ src/test/java/com/cydeo/tests/FirstClass.java
@@ -1,13 +1,15 @@
 package com.cydeo.tests;
 
+import com.github.javafaker.CreditCardType;
+import com.github.javafaker.Faker;
+
 public class FirstClass {
 
     public static void main(String[] args) {
 
         System.out.println("Hello World");
-
-
-
+        Faker faker = new Faker();
+        System.out.println(faker.finance().creditCard(CreditCardType.AMERICAN_EXPRESS));
 
     }
 
