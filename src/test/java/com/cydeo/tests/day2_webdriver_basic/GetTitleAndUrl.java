a600e4950bfdd91e94d4a01175e91e87ba5c499d
Bilal0313 <Bilalahmat222@gmail.com>
2023-11-13 20:54:34 -0500

    Updated the get title class

--- src/test/java/com/cydeo/tests/day2_webdriver_basic/GetTitleAndUrl.java
+++ src/test/java/com/cydeo/tests/day2_webdriver_basic/GetTitleAndUrl.java
@@ -1,12 +1,25 @@
 package com.cydeo.tests.day2_webdriver_basic;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class GetTitleAndUrl {
     public static void main(String[] args) {
 
+        WebDriverManager.chromedriver().setup();
 
+        WebDriver driver = new ChromeDriver();
 
+        driver.navigate().to("http://practice.cydeo.com");
 
+        String title = driver.getTitle();
+
+        System.out.println("title = " + title);
+
+        String currentUrl = driver.getCurrentUrl();
+
+        System.out.println("currentUrl = " + currentUrl);
 
     }
-
 }
