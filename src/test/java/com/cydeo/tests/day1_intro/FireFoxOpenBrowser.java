566b55f39d5699a4a6394c38aee7a57ae7a12199
Bilal0313 <Bilalahmat222@gmail.com>
2023-11-13 20:12:03 -0500

    Updated the FireFox browser

--- src/test/java/com/cydeo/tests/day1_intro/FireFoxOpenBrowser.java
+++ src/test/java/com/cydeo/tests/day1_intro/FireFoxOpenBrowser.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class FireFoxOpenBrowser {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
 
         WebDriverManager.firefoxdriver().setup();
