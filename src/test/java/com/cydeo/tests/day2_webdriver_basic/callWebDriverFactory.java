a0e50306149ad54be9d25d51fd86044a92ac1e85
Bilal0313 <Bilalahmat222@gmail.com>
2023-11-13 21:47:56 -0500

    Rename the Class

--- src/test/java/com/cydeo/tests/day2_webdriver_basic/callWebDriverFactory.java
+++ src/test/java/com/cydeo/tests/day2_webdriver_basic/callWebDriverFactory.java
@@ -2,7 +2,7 @@ package com.cydeo.tests.day2_webdriver_basic;
 
 import com.cydeo.utilities.WebDriverFactory;
 
-public class f {
+public class callWebDriverFactory {
 
     public static void main(String[] args) {
 
