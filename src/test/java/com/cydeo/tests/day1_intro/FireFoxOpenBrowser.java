9110ec5db48c6c888325630ca68b61974810c23f
Bilal0313 <Bilalahmat222@gmail.com>
2023-11-13 20:08:40 -0500

    Updated the FireFox browser

--- src/test/java/com/cydeo/tests/day1_intro/FireFoxOpenBrowser.java
+++ src/test/java/com/cydeo/tests/day1_intro/FireFoxOpenBrowser.java
@@ -1,10 +1,18 @@
 package com.cydeo.tests.day1_intro;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class FireFoxOpenBrowser {
 
     public static void main(String[] args) {
 
 
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.youtube.com/");
+        driver.close();
 
 
     }
