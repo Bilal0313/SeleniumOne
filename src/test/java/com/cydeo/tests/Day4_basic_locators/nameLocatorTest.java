e6b4c0dcd3bd6eca5e52568c50496743e90740b8
Bilal0313 <Bilalahmat222@gmail.com>
2023-11-14 15:14:14 -0500

    add the new class under package 4

---
+++ src/test/java/com/cydeo/tests/Day4_basic_locators/nameLocatorTest.java
@@ -0,0 +1,15 @@
+package com.cydeo.tests.Day4_basic_locators;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+
+public class nameLocatorTest {
+    public static void main(String[] args) {
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+
+
+
+
+    }
+}
