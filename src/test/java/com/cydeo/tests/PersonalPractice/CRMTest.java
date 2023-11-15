401970a5710dc156153059b4a2f8023893286e46
Bilal0313 <Bilalahmat222@gmail.com>
2023-11-15 15:10:51 -0500

    Personal Test Class add

---
+++ src/test/java/com/cydeo/tests/PersonalPractice/CRMTest.java
@@ -0,0 +1,38 @@
+package com.cydeo.tests.PersonalPractice;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.security.Key;
+
+public class CRMTest {
+
+
+
+
+    @Test
+    public void NagetiveTestOfLogin()throws InterruptedException{
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.get("https://login1.nextbasecrm.com/");
+        Thread.sleep(2000);
+
+        WebElement LoginBtnLocation = driver.findElement(By.xpath("//input[@class='login-inp'][1]"));
+        LoginBtnLocation.sendKeys("incorrect");
+
+        WebElement passWordLocation = driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+        passWordLocation.sendKeys("incorrect"+ Keys.ENTER);
+
+        String expectResult = "Incorrect login or password";
+        String actualResult = driver.findElement(By.cssSelector("div[class='errortext']")).getText();
+
+        Assert.assertEquals(expectResult,actualResult);
+
+    }
+
+}
---
+++ src/test/java/com/cydeo/tests/PersonalPractice/Day1/BackAndForth.java
@@ -0,0 +1,9 @@
+package com.cydeo.tests.PersonalPractice.Day1;
+
+public class BackAndForth {
+
+
+
+
+
+}
