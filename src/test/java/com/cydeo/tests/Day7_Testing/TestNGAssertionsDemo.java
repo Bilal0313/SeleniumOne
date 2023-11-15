0058b372dadfe36c746d21f316b32129437da048
Bilal0313 <Bilalahmat222@gmail.com>
2023-11-15 10:24:43 -0500

    Updated

--- src/test/java/com/cydeo/tests/Day7_Testing/TestNGAssertionsDemo.java
+++ src/test/java/com/cydeo/tests/Day7_Testing/TestNGAssertionsDemo.java
@@ -1,16 +1,32 @@
 package com.cydeo.tests.Day7_Testing;
 
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestNGAssertionsDemo {
 
 
+    @BeforeMethod
+    public void setUp(){
+        System.out.println("Open Browser");
+    }
 
 
     @Test
     public void test1 (){
-        Assert.assertEquals("title","title");
+        System.out.println("First Test");
+        Assert.assertEquals("title","title ");
+
+        System.out.println("Second Test");
+        Assert.assertEquals("url","url");
+
+    }
+
+    @AfterClass
+    public void done(){
+        System.out.println("Browser Close");
     }
 
 
