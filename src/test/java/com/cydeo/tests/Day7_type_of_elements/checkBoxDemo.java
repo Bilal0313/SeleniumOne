15075aa0e4b9355dacc0d8ae2fc04a093a48c040
Bilal0313 <Bilalahmat222@gmail.com>
2023-11-15 21:12:41 -0500

    Update the class

--- src/test/java/com/cydeo/tests/Day7_type_of_elements/checkBoxDemo.java
+++ src/test/java/com/cydeo/tests/Day7_type_of_elements/checkBoxDemo.java
@@ -9,17 +9,19 @@ import org.testng.annotations.Test;
 public class checkBoxDemo {
 
     @Test
-    public void test1(){
+    public void test1() throws InterruptedException {
         WebDriver driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.get("https://practice.cydeo.com/checkboxes");
 
         WebElement checkBox1 = driver.findElement(By.cssSelector("input[name='checkbox1'"));
         WebElement checkBox2 = driver.findElement(By.cssSelector("input[name='checkbox2'"));
-        // the checkbox is selected 
+        // the checkbox is selected
         System.out.println("checkBox1.isSelected() = " + checkBox1.isSelected());
         System.out.println("checkBox2.isSelected() = " + checkBox2.isSelected());
 
+        Thread.sleep(3000);
+        driver.quit();
 
     }
 
